2f943b5fddea57338811209bf32e44059c190d33
mohamed <mohamedseddik.hafidh@esprit.tn>
2026-04-22 18:06:36 +0100

    mocking rabbitmq and jenkins

--- Esprit-PiDev-4SAE5-2026-WallStreetEnglish-AuthProfile-MS-main/microservices/auth-service/src/test/java/tn/esprit/authservice/AuthServiceApplicationTests.java
+++ Esprit-PiDev-4SAE5-2026-WallStreetEnglish-AuthProfile-MS-main/microservices/auth-service/src/test/java/tn/esprit/authservice/AuthServiceApplicationTests.java
@@ -1,10 +1,18 @@
 package tn.esprit.authservice;
 
 import org.junit.jupiter.api.Test;
+import org.keycloak.admin.client.Keycloak;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class AuthServiceApplicationTests {
+
+    @MockitoBean  // ← Use this instead of @MockBean
+    private Keycloak keycloak;
+    @MockitoBean
+    private RabbitTemplate rabbitTemplate;
 
     @Test
     void contextLoads() {
--- Esprit-PiDev-4SAE5-2026-WallStreetEnglish-AuthProfile-MS-main/microservices/user-management-service/src/test/java/tn/esprit/usermanagementservice/UserManagementServiceApplicationTests.java
+++ Esprit-PiDev-4SAE5-2026-WallStreetEnglish-AuthProfile-MS-main/microservices/user-management-service/src/test/java/tn/esprit/usermanagementservice/UserManagementServiceApplicationTests.java
@@ -1,10 +1,14 @@
 package tn.esprit.usermanagementservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class UserManagementServiceApplicationTests {
+	@MockitoBean
+	private RabbitTemplate rabbitTemplate;
 
 	@Test
 	void contextLoads() {
